17a80d594824322e8c0980ead19d25d74b51f1ae
Lunara Cunha <lunara.cunha@hotmail.com>
2022-10-12 17:36:24 -0300

    instalação do nodemon --dev
    tutorial: como fazer upload de imagem, canal: Celke

--- upload_com_node/app.js
+++ upload_com_node/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const uploadUser = require('./middlewares/uploadimage');
 
-app.post("/upload-image", async(req,res) =>{
+app.post("/upload-image", uploadUser.single('image'), async(req,res) =>{
+if(req.file){
     return res.json({
         erro: false,
         mensagem: "upload realizado com sucesso!"
+    }); 
+}
+
+ return res.status(400).json({
+        erro:true,
+        mensagem: "Erro: upload não realizado"
     });
+
+   
 });
 
 app.listen(8080,() =>{
